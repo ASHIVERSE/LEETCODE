09f2a1050286467506c0a58e16fd25ce8417c122
ASHI SRIVASTAVA <ashi60515@gmail.com>
2026-06-15 00:12:57 +0530

    Time: 1 ms (78.65%) | Memory: 43.8 MB (24.28%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,33 +15,30 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
+            List<List<Integer>> result =new ArrayList<>();
+            if(root==null) return result;
+            Queue<TreeNode> q=new LinkedList<>();
+            q.add(root);
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+            boolean leftToRight=true;
+            while(!q.isEmpty())
+            {
+                int size=q.size();
+                List<Integer> level=new ArrayList<>();
+                for(int i=0;i<size;i++)
+                {
+                    TreeNode node=q.poll();
+                    level.add(node.val);
+                    if(node.left!=null) q.offer(node.left);
+                    if(node.right!=null) q.offer(node.right);
 
-        boolean leftToRight = true;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            Integer[] level = new Integer[size];
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-
-                int index = leftToRight ? i : size - 1 - i;
-                level[index] = node.val;
-
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
+                }
+                if(!leftToRight) Collections.reverse(level);
+                result.add(level);
+                 leftToRight = !leftToRight;
             }
+            return result;
 
-            result.add(Arrays.asList(level));
-            leftToRight = !leftToRight;
-        }
 
-        return result;
-    
     }
 }
