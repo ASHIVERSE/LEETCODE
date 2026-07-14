7edbd15131efdc2c295458d6c132c9163d30be5a
ASHI SRIVASTAVA <ashi60515@gmail.com>
2026-07-14 16:34:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (84.36%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+      // Step 1: Find breakpoint
+      int n=nums.length;
+    int index = -1;
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            index = i;
+            break;
+        }
+    }
+
+    // Already the largest permutation
+    if (index == -1) {
+        reverse(nums, 0, n - 1);
+        return;
+    }
+
+    // Step 2: Find smallest element greater than pivot
+    for (int i = n - 1; i > index; i--) {
+        if (nums[i] > nums[index]) {
+            swap(nums, i, index);
+            break;
+        }
+    }
+
+    // Step 3: Reverse the suffix
+    reverse(nums, index + 1, n - 1);
+}
+
+private void reverse(int[] nums, int left, int right) {
+
+    while (left < right) {
+        swap(nums, left, right);
+        left++;
+        right--;
+    }
+}
+
+private void swap(int[] nums, int i, int j) {
+
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+}
